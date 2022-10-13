d6cc3affc0ee058e63907242e62bebc13866e033
Omkar patil <omkarp666@gmail.com>
2022-10-13 18:55:07 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,52 +3,47 @@ import '../styles/App.css';
 import { IgnoreCaseToggle } from './IgnoreCaseToggle';
 import { ParagraphInput } from './ParagraphInput';
 import { WordInput } from './WordInput';
-
 const App = () => {
-  const [paragraphContent, setParagraphContent] = useState("");
-  const [search, setSearch] = useState(0)
+  const [word, setWord] = useState("")
+  const [wordCount,setWordCount] = useState(0)
   const ref = useRef()
-  const[ignoreCase, setIgnoreCase] = useState(false);
-  const highlight = (val, wordCase) =>{
+  const [ignoreCase, setIgnoreCase] = useState(false)
+  const highlight = (val,wordCase) =>{
     let newt = ''
-    let i =0
-    if(wordCase){
+    let i = 0
+    if (wordCase) {
       const reg = new RegExp(val, 'ig')
-      newt = ref.current.innerText,replaceAll(reg, (args)=>{
+      newt = ref.current.innerText.replaceAll(reg, (args) => {
         i++;
         return `<span class="highlighted-txt">${args}</span>`
       })
-    }else{
-      newt = ref.current.innerText.replaceAll(val, (args)=>{
+    } else {
+      newt = ref.current.innerText.replaceAll(val, (args) => {
         i++;
         return `<span class="highlighted-txt">${args}</span>`
       })
     }
-    setSearch(i)
+    setWordCount(i)
+
     ref.current.innerHTML = newt
   }
-
   const handleWordInput = (val) => {
     highlight(val,ignoreCase)
-    setParagraphContent(val)
+    setWord(val)
   }
-
   const handleChangeToggle = () =>{
     highlight(word,!ignoreCase)
     setIgnoreCase(!ignoreCase)
 
   }
-
   return (
     <div id="main">
-      <ParagraphInput pRef={ref}/>
-      <div>
-        Total matches: <span id="search-counter">{search}</span>
-      </div>
-      <WordInput handleChange={handleWordInput} value={paragraphContent} />
+      <ParagraphInput pRef={ref} />
+      <div>Total matches:- <span id="words-counter">{wordCount}</span></div>
+      <WordInput handleChange={handleWordInput} value={word} />
       <br />
       <IgnoreCaseToggle checked={ignoreCase} onChange={handleChangeToggle} />
-      <span id="search-counter">{search}</span>
+      <span id="words-counter">{wordCount}</span>
     </div>
   )
 }
